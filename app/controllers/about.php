7c0fd0d59acfdff35783c45647f7ac1428337b9e
Mikael <mikaelel902@gmail.com>
2025-05-19 19:23:15 +0700

    tambah controller

---
+++ app/controllers/about.php
@@ -0,0 +1,10 @@
+<?php
+
+class About{
+    public function index($nama = 'mikael', $hobi = 'bola'){
+        echo "hallo nama saya $nama, hobby saya adalah $hobi";
+    }
+    public function page(){
+        echo 'About/page';
+    }
+}
