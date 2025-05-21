183cff60af1a002a56eba52e3dad3bec6054dd8f
Mikael <mikaelel902@gmail.com>
2025-05-21 21:31:58 +0700

    model lanjutan

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,29 +1,25 @@
 <?php
 
 class Mahasiswa_model{
-    private $mhs =[
-        [
-        "nama" => "Mikael",
-        "nrp" => "235314036",
-        "email" => "mikael@gmail.com",
-        "jurusan" => "Informatika"
-        ],
-        [
-        "nama" => "Yin",
-        "nrp" => "235314037",
-        "email" => "yin@gmail.com",
-        "jurusan" => "Elektro"
-        ],
-        [
-        "nama" => "Mike",
-        "nrp" => "235314038",
-        "email" => "mike@gmail.com",
-        "jurusan" => "Mesin"
-        ],
-];
+    private $dbh;
+    private $stmt;
+
+    public function __construct(){
+        
+        // data source name
+        $dsn = 'mysql:host=localhost;dbname=phpmvc';
+
+        try{
+            $this->dbh = new PDO($dsn, 'root', '');
+        }catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
 
 
 public function getAllMahasiswa(){
-    return $this->mhs;
+    $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
+    $this->stmt->execute();
+    return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 }
