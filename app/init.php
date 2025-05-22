12e31ae0838b8c5a80cad7f7ba335dc537c7a203
Mikael <mikaelel902@gmail.com>
2025-05-22 14:27:09 +0700

    tambahkan flash Message

--- app/init.php
+++ app/init.php
@@ -3,6 +3,6 @@
 require_once 'core/App.php';//panggil data App yang ada di folder core
 require_once 'core/Controller.php';//panggil controller dari folder core
 require_once 'core/Database.php';//panggil database dari folder core
-
+require_once 'core/Flasher.php';
 
 require_once 'config/config.php';//panggil config dari folder config
